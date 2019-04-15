9025e0c63bbb575039bee5512cd9358e1b2e3517
jonssonmattias <1jonssonmattias@gmail.com>
2019-04-15 12:41:44 +0200

    Fixed confilct

--- src/controller/Authentication.java
+++ src/controller/Authentication.java
@@ -12,22 +12,12 @@ import mssql.MSSQL;
  *
  */
 public class Authentication {
-<<<<<<< HEAD
 	private MSSQL mssql;
 	private String database = "YOUR_DATABASE"; 	// Edit this
 	private String username = "YOUR_USERNAME"; 	// Edit this
 	private String password = "YOUR_PASSWORD"; 	// Edit this
 	private String hostname = "YOUR_HOSTNAME"; 	// Edit this
 	private String port 	= "YOUR_PORT";		// Edit this
-=======
-	private MySQL mysql;
-	private String database = "Testing"; // Edit this
-	private String username = "YOUR_USERNAME"; // Edit this
-	private String password = "YOUR_PASSWORD"; // Edit this
-	private String ipAddress = "127.0.0.1"; // localhost
-	private String port = "3306"; // Usual port for mysql
->>>>>>> branch 'develop' of https://github.com/Vandannh/CryptoFile.git
-	
 	/**
 	 * Sets up a connection to the database.
 	 */
