8ba24bb117e72d02e32e938a0d1b94406d3fcfcd
jonssonmattias <1jonssonmattias@gmail.com>
2019-05-07 15:49:32 +0200

    Fixed conflicts

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,12 +2,12 @@ package main.java.controller;
 
 import java.io.File;
 import java.util.ArrayList;
-<<<<<<< HEAD
 import javax.swing.*;
 import main.java.azure.AzureFileShareIO;
 import main.java.database.*;
 import mssql.MSSQL;
 import main.java.session.*;
+import main.java.text.*; 
 
 /**
  * Is used to control the events in the application 
@@ -25,7 +25,9 @@ public class Controller {
 	private String userid;
 	private MSSQL mssql;
 	private File file;
+	private SafeString safeString = new SafeString();
 	private Session session;
+
 	private final String connectionString = "YOUR_CONNECTION_STRING"; // Edit this
 
 	/**
@@ -42,8 +44,8 @@ public class Controller {
 	 * @return if the user can log in or not
 	 */
 	public boolean login(String username, String password) {
-		username = stripSlashes(escapeCharacters(username)).trim();
-		password = stripSlashes(escapeCharacters(password)).trim();
+		username = safeString.completeSafeString(username);
+		password = safeString.completeSafeString(password);
 		if(authentication.getAuthentication(username, password)) {
 			userid=mssql.select("users", new String[] {"id"}, "username='"+username+"'").replace("\t\t", "").trim();
 			azureFileShareIO.connect("user"+userid);
@@ -65,9 +67,9 @@ public class Controller {
 	 * a message telling the user it was created
 	 */
 	public ArrayList<String> register(String username, String email, String password) {
-		username = stripSlashes(escapeCharacters(username)).trim();
-		password = stripSlashes(escapeCharacters(password)).trim();
-		email = stripSlashes(escapeCharacters(email)).trim();
+		username = safeString.completeSafeString(username);
+		password = safeString.completeSafeString(password);
+		email = safeString.completeSafeString(email);
 		registration = new Registration(username, email, password);
 		ArrayList<String> messages = registration.register();
 		if(isNumeric(messages.get(0).toCharArray()[0])) {
@@ -112,7 +114,7 @@ public class Controller {
 			return filename+" has been downloaded";
 		return "An error occured. Download failed";
 	}
-	
+
 	public String deleteFile() {
 		String directory = chooseDirectory().toLowerCase();
 		String filename = JOptionPane.showInputDialog("Write file to Delete.(Including the file extension)");
@@ -130,35 +132,13 @@ public class Controller {
 	private boolean isNumeric(char ch) {
 		return ch>='0'&&ch<='9';
 	}
-	
+
 	private String chooseDirectory() {
 		JList<String> list = new JList<String>(new String[] {"Private", "Public"});
 		JOptionPane.showMessageDialog(null, list, "Choose directory", JOptionPane.PLAIN_MESSAGE);
 		return list.getSelectedValue();
 	}
-	
-	public String escapeCharacters(String input) {
-		StringBuilder sb = new StringBuilder();
-		for(char c : input.toCharArray()) {
-			switch(c){
-			case '<': sb.append("&lt;"); break;
-			case '>': sb.append("&gt;"); break;
-			case '\"': sb.append("&quot;"); break;
-			case '&': sb.append("&amp;"); break;
-			case '\'': sb.append("&apos;"); break;
-			default:
-				if(c>0x7e) {
-					sb.append("&#"+((int)c)+";");
-				}else
-					sb.append(c);
-			}
-		}
-		return sb.toString();
-	}
-	
-	public String stripSlashes(String input) {
-		return input.replace("\\", "");
-	}
+
 	public void unregisterUser() {
 		mssql.delete("users", "id="+userid);
 		mssql.delete("directory", "id="+userid);
@@ -179,132 +159,5 @@ public class Controller {
 					interrupt();
 				}
 		}
-=======
-import java.util.Arrays;
-
-import javax.swing.*;
-
-import main.java.azure.AzureFileShareIO;
-import main.java.database.*;
-import main.java.text.SafeString;
-import mssql.MSSQL;
-
-/**
- * Is used to control the events in the application 
- * 
- * @version 1.0
- * @since 2019-04-17
- * @author Mattias Jönsson & Robin Andersson
- *
- */
-public class Controller {
-	private AzureFileShareIO azureFileShareIO = new AzureFileShareIO();
-	private Authentication authentication = new Authentication();
-	private Registration registration;
-	private String userid;
-	private MSSQL mssql;
-	private File file;
-	private SafeString safeString = new SafeString();
-	private final String connectionString = "YOUR_CONNECTION_STRING"; // Edit this
-
-	/**
-	 * Connects to a MS SQL (SQL Server) database
-	 */
-	public Controller() {
-		mssql = new MSSQL(connectionString);
-	}
-	/**
-	 * Tries to log in the user
-	 * 
-	 * @param username the username the user is trying to log in with
-	 * @param password the password the user is trying to log in with
-	 * @return if the user can log in or not
-	 */
-	public boolean login(String username, String password) {
-		username = safeString.completeSafeString(username);
-		password = safeString.completeSafeString(password);
-		
-		if(authentication.getAuthentication(username, password)) {
-			azureFileShareIO.connect();
-			userid=mssql.select("users", new String[] {"id"}, "username='"+username+"'").replace("\t\t", "").trim();
-			return true;
-		}
-		else
-			return false;
-	}
-	/**
-	 * Tries to register the user
-	 * 
-	 * @param username the username the user is trying to register
-	 * @param email the email the user is trying to register 
-	 * @param password the password the user is trying to register
-	 * @return an ArrayList of the error messages if the user fail to register of 
-	 * a message telling the user it was created
-	 */
-	public ArrayList<String> register(String username, String email, String password) {
-		username = safeString.completeSafeString(username);
-		password = safeString.completeSafeString(password);
-		email = safeString.completeSafeString(email);
-		registration = new Registration(username, email, password);
-		ArrayList<String> messages = registration.register();
-		if(isNumeric(messages.get(0).toCharArray()[0])) {
-			azureFileShareIO.connect();
-			azureFileShareIO.createDirectoryInAzure(messages.get(0));
-			azureFileShareIO.createDirectoryInsideDirectoryInAzure(messages.get(0), "public");
-			azureFileShareIO.createDirectoryInsideDirectoryInAzure(messages.get(0), "private");
-			ArrayList<String> message = new ArrayList<String>();
-			message.add("User created");
-			return message;
-		}
-		else
-			return messages;
-	}
-	/**
-	 * Gets a file and tries to upload it
-	 * 
-	 * @return a String telling the user of the success of the upload 
-	 */
-	public String uploadFile() {
-		JFileChooser chooser = new JFileChooser();
-		int returnVal = chooser.showOpenDialog(null);
-		if(returnVal == JFileChooser.APPROVE_OPTION)
-			file = chooser.getSelectedFile();
-		if(file!=null) {
-			String choosenDirectory = chooseDirectory().toLowerCase();
-			String directoryId = mssql.select("directory", new String[] {"id"}, "name='"+choosenDirectory+"'");
-			azureFileShareIO.upload(userid,choosenDirectory	,file);
-			mssql.insert("directory", new String[] {"name","type","user_id","parent_id"}, new String[] {file.getName(),"file",userid,directoryId});
-			return file.getName()+" has been uploaded";
-		}
-		return "";
-	}
-	/**
-	 * Tries to download a file from Azure File Share
-	 * 
-	 * @return a String telling the user of the success of the download
-	 */
-	public String downloadFile(){
-		String directory = chooseDirectory().toLowerCase();
-		String filename = JOptionPane.showInputDialog("Write file to download.(Including the file extension)");
-		if(azureFileShareIO.download(userid,directory,filename))
-			return filename+" has been downloaded";
-		return "An error occured. Download failed";
-	}
-
-	/**
-	 * Checks if the character is a number
-	 * 
-	 * @param ch the character
-	 * @return if the character is a number or not
-	 */
-	private boolean isNumeric(char ch) {
-		return ch>='0'&&ch<='9';
-	}
-	
-	private String chooseDirectory() {
-		JList<String> list = new JList<String>(new String[] {"Private", "Public"});
-		JOptionPane.showMessageDialog(null, list, "Choose directory", JOptionPane.PLAIN_MESSAGE);
-		return list.getSelectedValue();
->>>>>>> refs/heads/feature/SQLInjections
 	}
 }
