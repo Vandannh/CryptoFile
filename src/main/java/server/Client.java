20b746f0eedeae6c2a324a9874de4fe4bbcbaf90
jonssonmattias <1jonssonmattias@gmail.com>
2019-05-09 17:03:42 +0200

    test

--- src/main/java/server/Client.java
+++ src/main/java/server/Client.java
@@ -16,7 +16,6 @@ public class Client extends JPanel implements ActionListener{
 	private ObjectOutputStream oos;
 	private ObjectInputStream ois;
 	private JButton btnSend = new JButton("Send");
-	private JButton btnRead = new JButton("Read");
 
 	public static void main(String argv[]) throws Exception {
 		JFrame frame = new JFrame( "Client" );
@@ -32,16 +31,12 @@ public class Client extends JPanel implements ActionListener{
 		ois = new ObjectInputStream(clientSocket.getInputStream());
 		setLayout(new GridLayout(1,2));
 		add(btnSend);
-		add(btnRead);
 		btnSend.addActionListener(this);
-		btnRead.addActionListener(this);
 	}
 	public void sendMessage() throws IOException {
 		Message msg = new Message(1,"Mattias","Hejsan1");
 		System.out.println(msg.getType());
 		oos.writeObject(msg);
-	}
-	public void readMessage() throws ClassNotFoundException, IOException {
 		new ListenFromServer().start();
 	}
 
@@ -54,24 +49,28 @@ public class Client extends JPanel implements ActionListener{
 				e1.printStackTrace();
 			}
 		}
-		else if(e.getSource()==btnRead) {
-			try {
-				readMessage();
-			} catch (IOException | ClassNotFoundException e1) {
-				e1.printStackTrace();
-			}
-		}
 	}
 	private class ListenFromServer extends Thread {
 		public synchronized void run() {
 			while(true) {
 				try {
 					Object obj = ois.readObject();
-					System.out.println(ois);
+					String str = obj.toString();
+					System.out.println(str);
 				} catch (ClassNotFoundException | IOException e) {
 					e.printStackTrace();
 				}
 			}
+		}
+		private String operation(Message msg) {
+			switch(msg.getType()) {
+			case Message.LOGIN:		return "You are logging in";
+			case Message.LOGOUT: 	return "You are logging out";
+			case Message.REGISTER: 	return "You are registering";
+			case Message.UPLOAD: 	return "You are uploading";
+			case Message.DOWNLOAD: 	return "You are downloading";
+			}
+			return null;
 		}
 	}
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -5,16 +5,15 @@ import java.net.*;
 import main.java.Message;
 
 public class Server {
-	private ObjectInputStream inFromClient;
-	private ObjectOutputStream outToClient;
 	private boolean running;
-	
+
 	public static void main(String argv[]) throws Exception {
 		Server server = new Server();
 		server.start();
 	}
-	
+
 	public void start() throws IOException, ClassNotFoundException {
+		System.out.println("Server started");
 		ServerSocket serverSocket = new ServerSocket(6789);
 		running=true;
 		while(running) {
@@ -24,32 +23,44 @@ public class Server {
 			t.start();
 		}
 	}
-	
+	public void stop() {
+		running=false;
+	}
+
 	private static String operation(Message msg) {
 		switch(msg.getType()) {
-		case Message.LOGIN: return "You are logging in";
-		case Message.LOGOUT: return "You are logging out";
-		case Message.REGISTER: return "You are registering";
-		case Message.UPLOAD: return "You are uploading";
-		case Message.DOWNLOAD: return "You are downloading";
+		case Message.LOGIN:		return "You are logging in";
+		case Message.LOGOUT: 	return "You are logging out";
+		case Message.REGISTER: 	return "You are registering";
+		case Message.UPLOAD: 	return "You are uploading";
+		case Message.DOWNLOAD: 	return "You are downloading";
 		}
 		return null;
 	}
 	private class ClientHandler extends Thread{
 		Socket socket;
-		ObjectInputStream sInput;
-		ObjectOutputStream sOutput;
+		ObjectInputStream ois;
+		ObjectOutputStream oos;
 		Message msg;
 		ClientHandler(Socket socket) throws ClassNotFoundException, IOException {
-			System.out.println("Server started");
-			while (!Thread.interrupted()) {
-				inFromClient = new ObjectInputStream(socket.getInputStream());
-				outToClient = new ObjectOutputStream(socket.getOutputStream());
-				msg = (Message) inFromClient.readObject();
-				System.out.println(operation(msg));
-				outToClient.writeObject(operation(msg).toUpperCase());
-			}
+			this.socket=socket;
 		}
-	
+		public void run() {
+			System.out.println("Client connected");
+			boolean running = true;
+			try {
+				ois = new ObjectInputStream(socket.getInputStream());
+				oos = new ObjectOutputStream(socket.getOutputStream());
+				while (running) {
+					Object obj = ois.readObject();
+					if(obj instanceof Message) {
+						msg = (Message)obj;
+					}
+					System.out.println(operation(msg));
+					oos.writeObject(operation(msg).toUpperCase());
+				}
+			} catch (IOException | ClassNotFoundException e) {}
+		}
+
 	}
 }
