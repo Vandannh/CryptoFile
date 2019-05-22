c825f7989343b4b3e04abfc992e80a69db43a65c
jonssonmattias <1jonssonmattias@gmail.com>
2019-05-22 21:35:06 +0200

    Commented SafeString.java

--- src/main/java/text/SafeString.java
+++ src/main/java/text/SafeString.java
@@ -1,6 +1,21 @@
 package main.java.text;
 
+/**
+ * This class is used to make a String safe
+ * 
+ * @version 1.0
+ * @since 2019-05-07
+ * @author Mattias Jönsson
+ *
+ */
 public class SafeString {
+	
+	/**
+	 * Escapes "bad" characters from a string
+	 * 
+	 * @param string A string you want to escape characters from
+	 * @return The safe string
+	 */
 	public static String escapeCharacters(String string) {
 		StringBuilder sb = new StringBuilder();
 		for(char c : string.toCharArray()) {
@@ -20,6 +35,13 @@ public class SafeString {
 		}
 		return sb.toString();
 	}
+	
+	/**
+	 * Unescapes a safe string
+	 * 
+	 * @param safeString A string you want to unescape characters from
+	 * @return The unescaped string
+	 */
 	public static String unescapeCharacters(String safeString) {
 		StringBuilder sb = new StringBuilder();
 		for(String s : safeString.split("&")) {
@@ -39,13 +61,32 @@ public class SafeString {
 		return sb.toString();
 	}
 
+	/**
+	 * Stripes a String of all forward slashes
+	 * 
+	 * @param string A string to strip the forward slashes from 
+	 * @return The string without forward slashes
+	 */
 	public static String stripForwardSlashes(String string) {
 		return string.replace("\\", "");
 	}
+	
+	/**
+	 * Stripes a string of all backward slashes
+	 * 
+	 * @param string A string to strip the backward slashes from
+	 * @return The string without backward slashes 
+	 */
 	public static String stripBackwardSlashes(String string) {
 		return string.replace("//", "");
 	}
 
+	/**
+	 * Trims the string
+	 * 
+	 * @param string A string to trim from
+	 * @return The trimmed string
+	 */
 	public static String trim(String string) {
 		string=string.trim();
 		int i=0;
@@ -56,7 +97,14 @@ public class SafeString {
 		}
 		return string;
 	}
-	public String completeSafeString(String string) {
+	
+	/**
+	 * Make a string completely safe
+	 * 
+	 * @param string A string to make safe 
+	 * @return The safe string
+	 */
+	public static String completeSafeString(String string) {
 		string = trim(string);
 		string = stripForwardSlashes(string);
 		string = stripBackwardSlashes(string);
